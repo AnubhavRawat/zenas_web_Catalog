923cf6d9f319aa5bfc789ed0051de5fce5eab438
AnubhavRawat <anubhavr248@gmail.com>
2024-02-02 08:34:25 +0530

    Update zenas_clothing_store.py

--- zenas_clothing_store.py
+++ zenas_clothing_store.py
@@ -1,1 +1,9 @@
+#new file
+import streamlit
+import pandas
+import requests
+import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
